3b9e2a1d2923850e93d9228362a1a5e69b45bace
Damien Mauger <mauger.damien@hotmail.fr>
2023-10-10 22:22:17 +0200

    feat: update tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,36 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      sans: ["Andika", "sans-serif"],
+    },
+    fontSize: {
+      base: "48px",
+    },
+
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          DEFAULT: "#011832",
+        },
+        secondary: {
+          DEFAULT: "#007bc2",
+        },
+        tertiary: {
+          DEFAULT: "#5c946e",
+        },
+        police: {
+          DEFAULT: "#ebebed",
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
